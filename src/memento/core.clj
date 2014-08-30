2a90ef45d40321234e62090a3e008b420e4967b3
Pat Thomas <pthomas@relaynetwork.com>
2014-08-30 12:38:46 -0400

    Whitespace.

--- src/memento/core.clj
+++ src/memento/core.clj
@@ -9,13 +9,16 @@
 
 (defmacro defregistry
   [registry-name & [{:keys [register-fn-alias trigger-fn-alias] :as opts}]]
-  (let [registry-atom-name (-> registry-name (str "-registry") symbol)
+  (let [registry-atom-name (-> registry-name
+                               (str "-registry")
+                               symbol)
         register-fn-name   (if-not register-fn-alias
                              (make-fn-name :register registry-name)
                              register-fn-alias)
         trigger-fn-name    (if-not trigger-fn-alias
                              (make-fn-name :trigger registry-name)
-                             trigger-fn-alias)]
+                    
+         trigger-fn-alias)]
     `(do
        ;; define registry
        (def ~registry-atom-name (atom {}))
