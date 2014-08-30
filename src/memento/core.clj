e4774523b8df979c8999427c20dde70087ab943e
Pat Thomas <pthomas@relaynetwork.com>
2014-08-30 00:10:43 -0400

    defregistry now takes optional options map: allows for generating names for register/trigger fns to be overwritten.

--- src/memento/core.clj
+++ src/memento/core.clj
@@ -10,12 +10,16 @@
       symbol))
 
 (defmacro defregistry
-  [registry-name]
+  [registry-name & [{:keys [register-fn-alias trigger-fn-alias] :as opts}]]
   (let [registry-atom-name (-> registry-name (str "-registry") symbol)
-        register-fn-name   (make-fn-name :register registry-name)
-        trigger-fn-name    (make-fn-name :trigger  registry-name)]
+        register-fn-name   (if-not register-fn-alias
+                             (make-fn-name :register registry-name)
+                             register-fn-alias)
+        trigger-fn-name    (if-not trigger-fn-alias
+                             (make-fn-name :trigger registry-name)
+                             trigger-fn-alias)]
     `(do
-       ;; define registry:    
+       ;; define registry
        (def ~registry-atom-name (atom {}))
 
        ;; define register!-fn
