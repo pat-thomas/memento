4d96de5e152af46645cfeec986d7905b4c315180
Pat Thomas <pthomas@relaynetwork.com>
2014-08-29 23:47:41 -0400

    Basic implementation of defregistry macro to define a registry and its functions to register handlers and trigger handlers.

--- src/memento/core.clj
+++ src/memento/core.clj
@@ -1,6 +1,21 @@
 (ns memento.core)
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defmacro defregistry
+  [registry-name]
+  (let [registry-atom-name (-> registry-name (str "-registry") symbol)
+        register-fn-name   (symbol (str "register-" (str registry-name) "-handler!"))
+        trigger-fn-name    (symbol (str "trigger-" (str registry-name) "-handler!"))]
+    `(do
+       ;; define registry:    
+       (def ~registry-atom-name (atom {}))
+
+       ;; define register!-fn
+       (defn ~register-fn-name
+         [handler-name# impl#]
+         (swap! ~registry-atom-name assoc handler-name# impl#))
+       
+       ;; define trigger!-fn
+       (defn ~trigger-fn-name
+         [handler-name# data#]
+         (when-let [handler# (get (deref ~registry-atom-name) handler-name#)]
+           (handler# data#))))))
