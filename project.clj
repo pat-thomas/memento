831e407240b40b20d650d85cb952c9583b357ad7
Pat Thomas <pthomas@relaynetwork.com>
2014-09-05 12:54:06 -0400

    0.2.0 -> 0.2.0-SNAPSHOT for development.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject memento "0.2.0"
+(defproject memento "0.2.0-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
